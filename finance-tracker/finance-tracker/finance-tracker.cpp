c0cffe375c16abe8538ede08fa1e2a3bdf738bb9
slaveyametodieva <slaveyamil@abv.bg>
2025-12-23 23:06:41 +0200

    Add setup functionality
    Added functionality to initialize the financial profile. Included verification for the number of months input to prevent invalid data.

--- finance-tracker/finance-tracker/finance-tracker.cpp
+++ finance-tracker/finance-tracker/finance-tracker.cpp
@@ -1,20 +1,28 @@
-// finance-tracker.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include<iostream>
+using std::cin;
+using std::cout;
+using std::endl;
 
-#include <iostream>
-
+const int MIN_MONTH = 1;
+const int MAX_MONTH = 12;
+bool validateMonths(int input)
+{
+    return input >= MIN_MONTH && input <= MAX_MONTH;
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "> setup" << '\n';
+    cout << "Enter number of months: ";
+
+    int months;
+    cin >> months;
+
+    if (!validateMonths(months)) 
+    {
+        cout << "The profile can not be created! Invalid input!" << '\n';
+        return -5;
+    }
+    cout << "Profile created successfully." << '\n';
+
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
