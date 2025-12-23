681e3243a1e82ffa273c5205410412be4886d782
slaveyametodieva <slaveyamil@abv.bg>
2025-12-23 23:16:50 +0200

    fixing setup
    Adding "count" to the function and variable. As the user should input count of months

--- finance-tracker/finance-tracker/finance-tracker.cpp
+++ finance-tracker/finance-tracker/finance-tracker.cpp
@@ -3,26 +3,28 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-const int MIN_MONTH = 1;
-const int MAX_MONTH = 12;
-bool validateMonths(int input)
+const int MIN_MONTH_COUNT = 1;
+const int MAX_MONTH_COUNT = 12;
+bool validateMonthsCount(int input)
 {
-    return input >= MIN_MONTH && input <= MAX_MONTH;
+    return input >= MIN_MONTH_COUNT && input <= MAX_MONTH_COUNT;
 }
 int main()
 {
     cout << "> setup" << '\n';
     cout << "Enter number of months: ";
 
-    int months;
-    cin >> months;
+    int monthsCount;
+    cin >> monthsCount;
 
-    if (!validateMonths(months)) 
+    if (!validateMonthsCount(monthsCount)) 
     {
         cout << "The profile can not be created! Invalid input!" << '\n';
         return -5;
     }
     cout << "Profile created successfully." << '\n';
+    cout << "\n\n";
+
 
 
 }
