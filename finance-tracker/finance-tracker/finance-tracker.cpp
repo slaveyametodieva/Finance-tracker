6cece012eaf94021c3bf2f6093952426b0b763c4
slaveyametodieva <slaveyamil@abv.bg>
2025-12-25 21:35:27 +0200

    command
    Adding certain commands. Made a strCmp for checking the inputed command and clearing some bugs. Started making the add command

--- finance-tracker/finance-tracker/finance-tracker.cpp
+++ finance-tracker/finance-tracker/finance-tracker.cpp
@@ -5,13 +5,56 @@ using std::endl;
 
 const int MIN_MONTH_COUNT = 1;
 const int MAX_MONTH_COUNT = 12;
+const int MAX_LENGTH_STR = 50;
+const int MIN_INCOME = 0;
+
+const char SETUP[] = {"setup"};
+const char ADD[] = { "add" };
+
+void clearCommand(char *command) {
+    if (command) {
+        *command = '\0';
+    }
+}
+bool strCmp(const char* input, const char* wantedCommand) {
+    if (!input) {
+        return false;
+    }
+    int i = 0;
+    while (input[i] != '\0' && wantedCommand[i] != '\0')
+    {
+        if (input[i] != wantedCommand[i])
+        {
+            return false;
+        }
+        i++;
+    }
+    if (input[i] == '\0' && wantedCommand[i] == '\0') {
+        return true;
+    }
+    return false;
+
+}
 bool validateMonthsCount(int input)
 {
     return input >= MIN_MONTH_COUNT && input <= MAX_MONTH_COUNT;
 }
+
+bool validateMonthRange(int month) {
+    return month >= MIN_MONTH_COUNT && month <= MAX_MONTH_COUNT;
+}
+
 int main()
 {
-    cout << "> setup" << '\n';
+    char command[MAX_LENGTH_STR];
+
+    cout << ">";
+    cin >> command;
+    if (!strCmp(command, SETUP)) {
+        return -1;
+    }
+    clearCommand(command);
+
     cout << "Enter number of months: ";
 
     int monthsCount;
@@ -19,12 +62,28 @@ int main()
 
     if (!validateMonthsCount(monthsCount)) 
     {
-        cout << "The profile can not be created! Invalid input!" << '\n';
+        cout << "The profile can not be created! Invalid months count!" << '\n';
         return -5;
     }
     cout << "Profile created successfully." << '\n';
     cout << "\n\n";
 
+    cout << "> " << '\n';
+    cin >> command;
+    if (!strCmp(command, ADD)) {
+        return -1;
+    }
+    clearCommand(command);
 
+    cout << "Enter month (1-"<<monthsCount<<"): ";
+    int month;
+    cin >> month;
+    if (!validateMonthRange(month))
+    {
+        cout << "Invalid month! The month should be in range from 1 to " << monthsCount<< '\n';
+        return -5;
+    }
+
+    
 
 }
